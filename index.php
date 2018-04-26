0f656e81b7127618c1be02cd0e80797c0ed98675
Tim <tferris88@gmail.com>
2018-04-25 21:00:08 -0400

    baby steps v2

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pollution Reporting</title>
+</head>
+<body>
+    
+<h1 class="type">Type of Pollution</h1>
+
+<form action = "" method = "post">
+
+<label for = "type">Air Pollution</label>
+<input type = "checkbox" name = "type">Smoke
+<input type = "checkbox" name = "type">Smog
+<input type = "checkbox" name = "type">Smell
+
+</form>
+
+</body>
+</html>
