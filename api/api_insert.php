59721c36c6bf9f642acb2ca072c1eb78b7da5354
Tim <tferris88@gmail.com>
2018-05-16 20:55:57 -0400

    still need to figure out clustering popups

--- api/api_insert.php
+++ api/api_insert.php
@@ -1,19 +1,25 @@
 <?php
+// including the database connection
 include "../db.php";
+// including a set of functions from another file
 include "../includes/functions.php";
 
+// if there is a POST value with the key 'report' in the HTTP, assign it to a variable, along with the other keys
 if (isSet($_POST['report'])) {
     $report = $_POST['report'];
     $lat = $_POST['latitude'];
     $lon = $_POST['longitude'];
 
+    // returns them to jquery for success function
     echo $report;
     echo $lat;
     echo $lon;
 
+    // inserts row into database 
     $query = "INSERT INTO `location` (`id`, `lat`, `lon`, `city`, `state`, `report`, `country`) VALUES (NULL, '{$lat}', '{$lon}', '', '', '{$report}', '') ";
     $add_report = mysqli_query($connect, $query);
     
+    // if the query is formatted correctly, it will run fine, if not, function will kill the code
     confirmQuery($add_report);
  }
 
--- includes/map.js
+++ includes/map.js
@@ -1,24 +1,30 @@
- 
+ // place for error message
 var x = document.getElementById("demo");
+// getting geolocation data
 function getLocation() {
+    // if it can find you, run this
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+        // if it cant find you, run this instead
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-
+// retreiving the latitude and longitude and coords
 function showPosition(position) {
     lat = position.coords.latitude;
     long = position.coords.longitude;
     // console.log(lat + " " + long);
+    // sticking the coords into an array for ease of access
     lat_long = [lat, long];
     
 }
 
 getLocation();
+
 var map = L.map('map').fitWorld();
 
+// calling on leaflet api for map
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -26,7 +32,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
     }).addTo(map);
-
+// centering the map and setting the default zoom
 map.locate({setView: true, maxZoom: 11});
 
 // function onLocationFound(e) {
@@ -40,22 +46,27 @@ map.locate({setView: true, maxZoom: 11});
 
 // map.on('locationfound', onLocationFound);
 
+// if it runs into any problems, will give error message
 function onLocationError(e) {
    alert(e.message);
 }
 
 map.on('locationerror', onLocationError);
-
+// when the page loads, run this
 $(document).ready(function() {
+    // get request to api
     $.get("api/api_test.php", function(data) {
+        // response from api, changes from string, into js object
             rawData = $.parseJSON(data);
             console.log(rawData);
+            // listing all the data from the database
             for (let x = 0; x < rawData.length; x++) {
                 latitude = rawData[x].lat;
                 longitude = rawData[x].lon;
                 // console.log("latitude: " + latitude + "\nlongitude: " + longitude);
+                // creating a marker with information from database
                 var marker = L.marker([latitude, longitude]).addTo(map);
-                marker.bindPopup(rawData[x].report);
+                marker.bindPopup("<b>Report:</b> " + rawData[x].report + "\n<b>Latitude:</b> " + rawData[x].lat + "\n<b>Longitude:</b> " + rawData[x].lon);
             }
                     // var marker = L.marker(rawData[x].lat, rawData[x].lon).addTo(map);
                 });
@@ -63,12 +74,16 @@ $(document).ready(function() {
             });
 
             $("#submit").on("click", function(event) {
+                // stopping the submit button from loading the page again
                 event.preventDefault();
+                // assigning the radio button that is checked to a variable
                 var potential = $("input[type='radio'][name='type']:checked");
+                // checked radio buttons value
                 var value = potential.val();
                 console.log(value);
                 console.log(lat_long);
                 $.ajax({
+                    // post request to api
                     type: "POST",
                     url: "api/api_insert.php",
                     data: {
