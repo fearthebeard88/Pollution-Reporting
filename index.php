1af7c97cec163bb8c95886935d90c94b118de175
Tim <tferris88@gmail.com>
2018-05-29 20:55:04 -0400

    i hate css

--- index.php
+++ index.php
@@ -27,6 +27,23 @@
 
 </div>
 
+<div class="mobile-form">
+
+<form id = "mobile-pollution" action = "api/api_insert.php" method = "post">
+
+    <label for = "m_type">Air Pollution: </label>
+        <div class="m_radio">
+            <input type="radio" name = "m_type" value = "smoke">Smoke
+            <input type="radio" name = "m_type" value = "smog">Smog
+            <input type="radio" name = "m_type" value = "smell">Smell
+            <div class = "m_submit">
+                <input type = "submit" name = "m_submit" value = "Report">
+            </div>
+        </div>
+</form>
+
+</div>
+
 
 <div id = "map">
 
